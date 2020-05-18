e15ef4efbf2482ac16676aa95c7d65a4a95b4448
shriram-mnit <shriram969460@gmail.com>
2020-05-18 17:37:11 +0000

    add copy button

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React ,{Component}from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
 import ImageLinkForm from'./components/imageLinkForm';
+import copy from "copy-to-clipboard";
 
 const particlesOptions = {
   "particles": {
@@ -18,10 +19,16 @@ class App extends Component {
      constructor() {
     super();
     this.state = {
-        route:"",
+        route:"url",
         url:"",
-        short:""
+        short:"",
+        btnText:"copy short url"
     }}
+
+    copyCodeToClipboard = () => {
+    copy(this.state.short);
+    this.setState({ btnText: "Copied! url" });
+  }
     
     onUrlChange = (event) => {
     this.setState({url: event.target.value});
@@ -42,6 +49,7 @@ class App extends Component {
       .then(url => {
         if (url.shortUrl) {
           this.setState({short:url.shortUrl})
+          this.onRouteChange('shortUrl')
           console.log(this.state.short)
         }
       })
@@ -56,8 +64,13 @@ render(){
      <Particles className='particles'
           params={particlesOptions}
         />
+        {this.state.route==='url'?<ImageLinkForm shorturl={this.state.short}onChange={this.onUrlChange} onSubmit={this.onSubmit} />:<button onClick={() => this.copyCodeToClipboard()}>
+            {this.state.btnText}
+          </button>
+        }
         <p>{this.state.short}</p>
-   <ImageLinkForm shorturl={this.state.short}onChange={this.onUrlChange} onSubmit={this.onSubmit} />
+        
+   
 </div>
   );
    } }
